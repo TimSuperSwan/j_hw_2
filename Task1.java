e186d175795573d4dfbbdd704249b992b396cede
Artem <lebartigor@gmail.com>
2023-02-18 17:03:29 +0300

    сделал первое задание

--- Task1.java
+++ Task1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*В файле содержится строка с исходными данными в такой форме:
 {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
 Требуется на её основе построить и вывести на экран новую строку, в форме SQL запроса:
@@ -5,7 +7,33 @@ SELECT * FROM students WHERE name = "Ivanov" AND country = "Russia" AND city = "
 Для разбора строки используйте String.split. 
 Сформируйте новую строку, используя StringBuilder. Значения null не включаются в запрос. */
 public class Task1 {
+    public static void main(String[] args) {
+        String taskString = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        taskString = taskString.substring(1, taskString.length()-1);
 
+        String[] arrayTaskString = taskString.split(", ", 0);
+        System.out.println(Arrays.toString(arrayTaskString));
 
-    
+        StringBuilder resultBuilder =  new StringBuilder();
+        resultBuilder.append("SELECT * FROM students WHERE ");
+        
+        boolean count = true;
+        for (String string : arrayTaskString) {
+            String[] keyValue = string.split(":", 0);
+            String key = keyValue[0];
+            key = key.replace("\"", "");
+            String value = keyValue[1];
+            if (!count) {
+                resultBuilder.append(" AND ");
+            }
+            resultBuilder.append(String.format("%s = %s", key, value));
+            count = false;
+            
+            
+
+        }
+
+        System.out.println(resultBuilder.toString());
+    }
+
 }
